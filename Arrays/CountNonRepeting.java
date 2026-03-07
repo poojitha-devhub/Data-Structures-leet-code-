160d01a1f978977ddc4c67c73b8ea7f6a62cc88b
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-03-07 10:30:10 +0530

    Added first non repeting number

---
+++ Arrays/CountNonRepeting.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class CountNonRepeting {
+    public int firstNonrepeting(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            boolean found=false;
+            for(int j=0;j<n;j++){
+                if(arr[i]==arr[j]&&i!=j){
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int arr[]={-1,2,-1,5,2,3,2,4,4};
+        CountNonRepeting obj=new CountNonRepeting();
+        int result=obj.firstNonrepeting(arr);
+        System.out.println(result);
+
+    }
+    
+}
