f27f11a8927ccc90449b3f8c060da71b376676ae
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-03-24 20:07:26 +0530

    Added implementation of linked list

---
+++ LinkedLists/impliLL.java
@@ -0,0 +1,15 @@
+package LinkedLists;
+import java.util.*;
+
+public class impliLL {
+    public static void main(String[] args) {
+        LinkedList<String> List=new LinkedList<>();
+        List.addFirst("a");
+        List.addFirst("is");
+        System.out.println(List);
+        List.addLast("List");
+        System.out.println(List);
+
+    }
+    
+}
