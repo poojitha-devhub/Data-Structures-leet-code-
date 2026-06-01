bec47047e9f035e6de7ef97676f5278ec5f75266
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-06-01 21:42:18 +0530

    Added pattern 6

---
+++ Numbers/Pattern6.java
@@ -0,0 +1,23 @@
+package Numbers;
+
+public class Pattern6 {
+    public static void rev_pyramid(int rows,int cols){
+        for (int i=1;i<=rows;i++){
+            //spaces
+            for(int j=1;j<=(rows-i);j++){
+                System.out.print("   ");
+            }
+            //stars
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int rows=4;
+        int cols=4;
+        rev_pyramid(rows, cols);
+    }
+    
+}
