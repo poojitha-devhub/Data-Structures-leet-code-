415635cbf0864cff996182ec34c5c87648f22903
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-06-01 21:51:58 +0530

    modified pattern 6

--- Numbers/Pattern6.java
+++ Numbers/Pattern6.java
@@ -5,18 +5,18 @@ public class Pattern6 {
         for (int i=1;i<=rows;i++){
             //spaces
             for(int j=1;j<=(rows-i);j++){
-                System.out.print("   ");
+                System.out.print(" ");
             }
             //stars
             for(int j=1;j<=i;j++){
-                System.out.print(" * ");
+                System.out.print(i + " ");
             }
             System.out.println();
         }
     }
     public static void main(String[] args) {
-        int rows=4;
-        int cols=4;
+        int rows=5;
+        int cols=5;
         rev_pyramid(rows, cols);
     }
     
