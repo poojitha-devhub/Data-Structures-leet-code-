5aacae34895e1c36bc2e1a84a410af5b7612be8a
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-06-04 15:10:08 +0530

    modified pairs

--- Arrays/Pairs.java
+++ Arrays/Pairs.java
@@ -2,12 +2,15 @@ package Arrays;
 
 public class Pairs {
     public static void printPairs(int arr[]){
+        int count=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
                 System.out.print("("+arr[i]+","+arr[j]+") ");
+                count++;
             }
             System.out.println();
         }
+        System.out.print("total pairs= "+count);
     }
     public static void main(String[] args) {
         int arr[]={2,4,6,8,10};
