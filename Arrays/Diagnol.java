20bdb0b95e37140c6378f8f3fefaaa7701e96642
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-06-14 13:57:44 +0530

    Added Diagnol sum of 2D matrix

---
+++ Arrays/Diagnol.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+public class Diagnol {
+    public static void diagnolSum(int matrix[][]){
+        int n=matrix.length;
+        int m=matrix[0].length;
+        //left
+        int j=0;
+        int leftsum=0;
+        for(int i=0;i<n;i++){
+            if(i==j){
+                leftsum+=matrix[i][j];
+            }
+            j++;
+        }
+        //right
+        int s=m-1;
+        int rightsum=0;
+        int p=0;
+        while(s>=0){
+            rightsum+=matrix[p][s];
+            s--;
+            p++;
+        }
+        System.out.println("left sum: "+ leftsum);
+        System.out.println("right sum: "+ rightsum);
+        System.out.println("total sum: " + (leftsum+rightsum));
+
+    }
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        diagnolSum(matrix);
+    }
+    
+}
