e8ae2ea894f41d20f2369da96e48a1603fd2c466
Sri poojitha <121282475+srichimmy@users.noreply.github.com>
2026-06-26 12:08:15 +0530

    Added Uppercase to Lower Case

---
+++ BitManipulation/upperToLower.java
@@ -0,0 +1,13 @@
+package BitManipulation;
+
+public class upperToLower {
+    public static void main(String[] args) {
+        char a='A';
+        int ans=(char)a|32;
+        a=(char)ans;
+        System.out.println(a);
+
+
+    }
+    
+}
